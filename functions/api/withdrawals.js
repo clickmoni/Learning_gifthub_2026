07fae4e131343433211fd9e5d8e808ca2d249053
clickmoni <brightshedrack06@gmail.com>
2026-07-15 05:03:41 +0100

    Update withdrawals.js

--- functions/api/withdrawals.js
+++ functions/api/withdrawals.js
@@ -1,33 +1,45 @@
 export async function onRequestGet(context) {
-  const { env } = context;
 
-  try {
-    const { results } = await env.DB.prepare(`
-      SELECT
-    withdrawals.id,
-    users.email,
-    withdrawals.amount,
-    withdrawals.bank_name,
-    withdrawals.account_name,
-    withdrawals.account_number,
-    withdrawals.status,
-    withdrawals.created_at
-FROM withdrawals
-LEFT JOIN users
-ON withdrawals.user_id = users.id
-ORDER BY withdrawals.created_at DESC
-        
-    `).all();
+    const { env } = context;
 
-    return Response.json({
-      success: true,
-      withdrawals: results
-    });
+    try {
 
-  } catch (err) {
-    return Response.json({
-      success: false,
-      message: err.message
-    });
-  }
+        const withdrawals = await env.DB.prepare(`
+            SELECT
+                w.id,
+                w.amount,
+                w.status,
+                w.created_at,
+
+                u.first_name,
+                u.last_name,
+                u.email,
+                u.plan,
+                u.bank_name,
+                u.account_number,
+                u.social_platform,
+                u.social_link
+
+            FROM withdrawals w
+
+            LEFT JOIN users u
+            ON w.user_id = u.id
+
+            ORDER BY w.id DESC
+        `).all();
+
+        return Response.json({
+            success: true,
+            withdrawals: withdrawals.results
+        });
+
+    } catch (err) {
+
+        return Response.json({
+            success: false,
+            message: err.message
+        });
+
+    }
+
 }
